df67b482104bcc6f0fffe43df13c30a4f0bc231c
Jarom D Tollefson <35904250+JaromTollefson@users.noreply.github.com>
2019-05-01 22:25:12 -0600

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,4 +1,3 @@
-//C867 Project -- student.cpp
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,9 +5,6 @@
 using namespace std;
 
 #include "student.h"
-
-
-//#include "degree.h"
 
 const string studentData[] =
 { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
@@ -20,25 +16,20 @@ const string studentData[] =
 std::vector<Student*> classStudentArray;
 string studentDataStrings[9];
 
-Student::Student() { } //required
+Student::Student() { } 
 Student::Student(string *s) :
 
 	studentId(s[0]),
 	firstName(s[1]),
 	lastName(s[2]),
 	emailAdrs(s[3]),
-	age(atoi(s[4].c_str())), //ASCII to integer
+	age(atoi(s[4].c_str())), 
 	numDays1(s[5]),
 	numDays2(s[6]),
 	numDays3(s[7])
-	//	degreeName(s[8]),
-	//	degreeType(notSpecified)
 {}
 
-//void Student::print() {};
 void Student::getDegreeProgram() {};
-
-//virtual print to print Student data
 void Student::print()
 {
 	cout << "Student ID: " << studentId << '\t'
@@ -50,16 +41,8 @@ void Student::print()
 		<< endl;
 }
 
-//destructor
+
 Student::~Student() { }
-
-						//virtual getDegreeProgram
-						/* string Student::getDegreeProgram() {
-						return "";
-						}
-						*/
-
-						//  parses studentData[] into studentDataStrings[]
 void setup(string str) {
 	int firstCharPosition = 0;
 	int x = 0;
@@ -72,15 +55,11 @@ void setup(string str) {
 			studentDataStrings[x] = str.substr(firstCharPosition, length);
 			x++;
 			firstCharPosition = i + 1;
-			//DEBUG cout << studentDataStrings[x-1] << endl;
 		}
 	}
 	int length = i - firstCharPosition;
 	studentDataStrings[x] = str.substr(firstCharPosition, length);
-	//DEBUG cout << studentDataStrings[x] << endl;
 
-	//Student data is now parsed into an array of 9 strings
-	//Depending on the type, setup proper object
 	Student *pObj = 0;
 	if (studentDataStrings[8] == "SECURITY") {
 		pObj = initSec(studentDataStrings);
@@ -91,11 +70,7 @@ void setup(string str) {
 	else if (studentDataStrings[8] == "SOFTWARE") {
 		pObj = initSof(studentDataStrings);
 	}
-//	else { //if unknown
-//		pObj = new Student(studentDataStrings);
-//	}
 	classStudentArray.push_back(pObj);
-	//pObj->print(); //DEBUG
 }
 void setupStudent() {
 	for (int i = 0; i < 5; i++)
