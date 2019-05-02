5b68624a41afd49c51f3db31c24a38f6ce365050
Jarom D Tollefson <35904250+JaromTollefson@users.noreply.github.com>
2019-05-01 21:43:02 -0600

    Create securityStudent.h

---
+++ securityStudent.h
@@ -0,0 +1,13 @@
+    
+#pragma once
+#include "degree.h"
+#include "student.h"
+
+class SecurityStudent : public Student
+{
+protected:
+	Degree degree = SECURITY;
+public:
+	using Student::Student;
+	Degree getDegree() override;
+}
